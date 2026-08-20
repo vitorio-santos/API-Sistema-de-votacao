6ca6d2a3a268a35d257c83218f664b3ddb8d9fa4
Vitório Santos <124390765+vitorio-santos@users.noreply.github.com>
2026-08-20 14:50:32 -0300

    Update Voto.java

--- sistema-de-votacao/src/main/java/infrastructure/entity/Voto.java
+++ sistema-de-votacao/src/main/java/infrastructure/entity/Voto.java
@@ -1,4 +1,4 @@
-package infrastructure.entity;
+package com.VitorioSantos.sistema_de_votacao.infrastructure.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -9,9 +9,15 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
-@Table(uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"usuario_id", "enquete_id"})
-})
+@Table(
+        name = "votos",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_voto_usuario_enquete",
+                        columnNames = {"usuario_id", "enquete_id"}
+                )
+        }
+)
 public class Voto {
 
     @Id
@@ -19,13 +25,17 @@ public class Voto {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
 
     @ManyToOne
+    @JoinColumn(name = "enquete_id", nullable = false)
     private Enquete enquete;
 
     @ManyToOne
+    @JoinColumn(name = "opcao_id", nullable = false)
     private OpcaoVoto opcao;
 
+    @Column(name = "data_voto", nullable = false)
     private LocalDateTime dataVoto;
 }
